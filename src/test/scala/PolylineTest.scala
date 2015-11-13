2815f3b1015187bafb0e88b6b0512d163e78a3cc
Christopher Poenaru <kiambogo@gmail.com>
2015-11-12 16:45:11 -0800

    added test that breaks

--- src/test/scala/PolylineTest.scala
+++ src/test/scala/PolylineTest.scala
@@ -3,21 +3,26 @@ import org.scalatest.WordSpec
 
 class PolylineTest extends WordSpec {
   val pl = new Polyline
-  val coordinates = List(
+  val coordinates1 = List(
     LatLng(38.5, -120.2),
     LatLng(40.7, -120.95),
     LatLng(43.252, -126.453)
   )
+  val coordinates2 = List(LatLng(43.525529, -80.288606))
 
   "A Polyline" when {
     "encoding" should {
-      "encode a list of coordinates to a polyline string" in {
-        assert(pl.encode(coordinates) == "_p~iF~ps|U_ulLnnqC_mqNvxq`@")
+      "encode a list of coordinates to a polyline string (#1)" in {
+        assert(pl.encode(coordinates1) == "_p~iF~ps|U_ulLnnqC_mqNvxq`@")
+      }
+      "encode a list of coordinates to a polyline string (#2)" in {
+        assert(pl.encode(coordinates2) == "qathGxj`iN")
       }
     }
+
     "decoding" should {
-      "decode a polyline string to a list of coordinates" in {
-        assert(pl.decode("_p~iF~ps|U_ulLnnqC_mqNvxq`@") == coordinates)
+      "decode a polyline string to a list of coordinates (#1)" in {
+        assert(pl.decode("_p~iF~ps|U_ulLnnqC_mqNvxq`@") == coordinates1)
       }
     }
   }
