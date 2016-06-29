57b4f826a87838ce7eda6c981bdd0579ddf9a521
Christopher Poenaru <kiambogo@gmail.com>
2016-06-28 22:42:08 -0400

    Fixed issues arising from too high precision points

--- src/test/scala/main/PolylineTest.scala
+++ src/test/scala/main/PolylineTest.scala
@@ -1,3 +1,5 @@
+package main
+
 import main.{Polyline, LatLng}
 import org.scalatest.WordSpec
 
@@ -6,9 +8,11 @@ class PolylineTest extends WordSpec {
   val coordinates1 = List(
     LatLng(38.5, -120.2),
     LatLng(40.7, -120.95),
-    LatLng(43.252, -126.453)
+    LatLng(43.252, -126.453))
+  val coordinates2 = List(
+    LatLng(43.525524, -80.288602),
+    LatLng(44.285024, -81.294820)
   )
-  val coordinates2 = List(LatLng(43.525529, -80.288606))
 
   "A Polyline" when {
     "encoding" should {
@@ -16,13 +20,19 @@ class PolylineTest extends WordSpec {
         assert(pl.encode(coordinates1) == "_p~iF~ps|U_ulLnnqC_mqNvxq`@")
       }
       "encode a list of coordinates to a polyline string (#2)" in {
-        assert(pl.encode(coordinates2) == "qathGxj`iN")
+        assert(pl.encode(coordinates2) == "oathGvj`iN{isCzocE")
       }
     }
 
     "decoding" should {
       "decode a polyline string to a list of coordinates (#1)" in {
         assert(pl.decode("_p~iF~ps|U_ulLnnqC_mqNvxq`@") == coordinates1)
+      }
+      "decode a polyline string to a list of rounded coordinates (#2)" in {
+        assert(pl.decode("oathGvj`iN{isCzocE") == List(
+          LatLng(43.52552, -80.28860),
+          LatLng(44.28502, -81.29482)
+        ))
       }
     }
   }
